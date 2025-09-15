97f3e7ce0d4d418842ca7dc437477cc6fa5b63e2
Orne2004 <ornellasarantes@gmail.com>
2025-09-15 17:30:34 -0300

    Refactor nombres de funciones a formato Camel Case

--- src/ghostbusters.js
+++ src/ghostbusters.js
@@ -7,7 +7,7 @@ class Ghost {
 
 let ghosts = [];
 
-const create_ghost = (ghost) => {
+const add_ghost = (ghost) => {
   ghosts.push({
     name: ghost.name,
     dangerLevel: ghost.dangerLevel,
@@ -15,14 +15,14 @@ const create_ghost = (ghost) => {
   });
 };
 
-const set_DANGER_LEVEL = (name, level) => {
+const setDangerLevel = (name, level) => {
   const ghost = ghostReports.find((g) => g.name === name);
   if (ghost) {
     ghost.dangerLevel = level;
   }
 };
 
-const CAPTUREGHOST3 = (name) => {
+const captureGhost = (name) => {
   const ghost = ghostReports.find((g) => g.name === name);
   if (ghost) {
       ghost.captured = true;
